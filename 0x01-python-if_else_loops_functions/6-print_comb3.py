b3a92f1fe8ee8f376fc75d4b989dec877b42e1e1
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-03-08 19:31:35 +0000

    0x01 task01

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end="")
+        if i != 8 or j != 9:
+            print(", ", end="")
+print()
