22d91232c2366a9c053f8aa1d4a5b2cbb98d6a34
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-03-09 18:44:10 +0000

    0x02 task02

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,13 +2,13 @@
 
 if __name__ == '__main__':
     # import module
-    import calculator_1 as cal
+    import calculator_1 
 
     a = 10
     b = 5
 
-    print(f"{a} + {b} = {cal.add(a, b)}")
-    print(f"{a} - {b} = {cal.sub(a, b)}")
-    print(f"{a} * {b} = {cal.mul(a, b)}")
-    print(f"{a} / {b} = {cal.div(a, b)}")
+    print(f"{a} + {b} = {calculator_1.add(a, b)}")
+    print(f"{a} - {b} = {calculator_1.sub(a, b)}")
+    print(f"{a} * {b} = {calculator_1.mul(a, b)}")
+    print(f"{a} / {b} = {calculator_1.div(a, b)}")
 
