c462bc898408023c7bbacb4a121198001fd27ffc
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-03-09 19:10:31 +0000

    0x02 task03

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+
+    args = 0
+    for i in range(len(sys.argv) - 1):
+        args += int(sys.argv[i + 1])
+    print("{}".format(args))
