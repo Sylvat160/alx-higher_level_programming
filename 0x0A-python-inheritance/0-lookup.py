2e520b34e8c45bca4e8566284d879d934835b508
Sylvain T <sylvaintagnabou@gmail.com>
2023-04-11 19:15:28 +0000

    0x0A task00

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Contains the lookup function
+"""
+
+def lookup(obj):
+    """
+    returns a list of available attributes
+    & method of an object
+    """
+    return (obj)
