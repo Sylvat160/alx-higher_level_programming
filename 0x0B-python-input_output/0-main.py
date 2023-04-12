d609e9f0a8db90148b122d64d2f4d8cbf0cf199c
Sylvain T <sylvaintagnabou@gmail.com>
2023-04-12 22:20:15 +0000

    0X0B task00

---
+++ 0x0B-python-input_output/0-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+readFile = __import__('0-read_file').read_file
+
+readFile("README.md")
---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a function that reads a text
+file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
