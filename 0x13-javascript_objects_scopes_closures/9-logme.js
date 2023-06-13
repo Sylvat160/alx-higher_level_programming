893dd0656fb2b76b06990a1b4140511a319bf80d
Sylvain T <sylvaintagnabou@gmail.com>
2023-06-13 20:34:50 +0000

    0x13 task09_10

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.logMe = function (item) {
+  if (this.count === undefined) {
+    this.count = 0;
+  } else {
+    this.count++;
+  }
+  console.log(this.count + ': ' + item);
+};
