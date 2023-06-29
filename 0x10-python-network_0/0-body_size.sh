7977a45acf3cbef569b838f4c892db800e4d9c2c
Sylvain T <sylvaintagnabou@gmail.com>
2023-06-29 20:48:07 +0000

    0000

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,11 @@
 # Write a Bash script that takes in a URL, sends a request
 # to that URL, and displays the size of the body of the response
 
+# Check if URL argument is provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Send the request using curl, pipe the output to wc to count the bytes, and display the result
 curl -s "$1" | wc -c
